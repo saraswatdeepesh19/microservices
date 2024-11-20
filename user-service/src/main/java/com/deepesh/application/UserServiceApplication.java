8b8b58640fc9dbd38fddcdb5fe20af228f2fcaaf
saraswatdeepesh19 <saraswatdeepesh19@gmail.com>
2024-11-20 19:36:50 +0530

    initial commit

---
+++ user-service/src/main/java/com/deepesh/application/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deepesh.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
